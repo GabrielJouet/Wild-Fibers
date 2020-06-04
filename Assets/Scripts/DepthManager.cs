d20b40b5228b34eae77e906fe59dd5a2dd59677e
Gabriel Jouet <gabriel.jouet@viacesi.fr>
2020-06-04 17:50:26 +0200

    Added a new script for depth management #50
    Needs to be implement on litterally everything (not on Background and path though)

---
+++ Assets/Scripts/DepthManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class DepthManager : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer _spriteRenderer;
+
+    private Camera _mainCamera;
+
+    private void Start()
+    {
+        _mainCamera = Camera.main;
+    }
+
+
+    private void FixedUpdate()
+    {
+        _spriteRenderer.sortingOrder = (int)_mainCamera.WorldToScreenPoint(transform.position).y * -1;
+    }
+}
