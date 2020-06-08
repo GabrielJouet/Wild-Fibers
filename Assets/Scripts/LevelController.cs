dc6e651d98de28d3745662a6d6a125812691d891
Gabriel Jouet <gabriel.jouet@viacesi.fr>
2020-06-08 21:04:31 +0200

    Added a level controller and spawners

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    [SerializeField]
+    private Level _level;
+
+    private int _waveIndex = 0;
+
+    [SerializeField]
+    private Spawner _spawnerPrefab;
+    private List<Spawner> _spawners;
+
+
+    private void Start()
+    {
+        Debug.Log("Waves number " + _level.GetWaveCount());
+        Debug.Log("Waves " + _waveIndex + " / " + _level.GetWaveCount());
+
+        StartWave();
+    }
+
+
+    private void StartWave()
+    {
+        int spawnerLeft = _level.GetWave(_waveIndex).GetNumberOfEnemyGroup() - _spawners.Count;
+
+        for(int i = 0; i < spawnerLeft; i ++)
+            InstantiateSpawners();
+
+        for(int i = 0 ; i < _level.GetWave(_waveIndex).GetNumberOfEnemyGroup(); i ++)
+            _spawners[i].SetNewGroup(_level.GetWave(_waveIndex).GetEnemyGroup(i));
+    }
+
+
+    private void InstantiateSpawners()
+    {
+        _spawners.Add(Instantiate(_spawnerPrefab));
+    }
+
+
+    public void EndWave()
+    {
+        bool result = true;
+
+        foreach (Spawner current in _spawners)
+            if (!current.GetCanSpawn())
+                result = false;
+
+        if(result)
+            StartCoroutine(DelayWave());
+    }
+
+
+    private IEnumerator DelayWave()
+    {
+        if (_waveIndex < _level.GetWaveCount())
+        {
+            _waveIndex++;
+
+            yield return new WaitForSeconds(_level.GetWave(_waveIndex).GetTimeBeforeNextWave());
+
+            StartWave();
+        }
+        else
+            Debug.Log("fini");
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    private EnemyGroup _enemyGroup;
+
+    private int _patternIndex = 0;
+    private int _enemyIndex = 0;
+
+    private bool _canSpawn = true;
+
+    public void SetNewGroup(EnemyGroup newGroup)
+    {
+        _enemyGroup = newGroup;
+
+        StartCoroutine(SpawnEnemies());
+    }
+
+
+    private IEnumerator SpawnEnemies()
+    {
+        while(_canSpawn)
+        {
+            if(_enemyIndex < _enemyGroup.GetEnemyPattern(_patternIndex).GetNumberOfEnemies())
+            {
+                _enemyIndex ++;
+                Instantiate(_enemyGroup.GetEnemyUsed());
+                yield return new WaitForSeconds(_enemyGroup.GetEnemyPattern(_patternIndex).GetTimeBetweenEnemies());
+            }
+            else
+            {
+                if (_patternIndex < _enemyGroup.GetEnemyPatternCount())
+                    _patternIndex++;
+                else
+                    EndSpawn();
+
+                yield return new WaitForSeconds(_enemyGroup.GetTimeBetweenPattern());
+            }
+        }
+    }
+
+
+    private void EndSpawn()
+    {
+        _patternIndex = 0;
+        _enemyGroup = null;
+        _canSpawn = false;
+        StopAllCoroutines();
+
+        FindObjectOfType<LevelController>().EndWave();
+    }
+
+
+    public bool GetCanSpawn() { return _canSpawn; }
+}
