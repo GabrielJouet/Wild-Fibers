d13e69d2a18c690aaf22f687b1ef9865fe29c0d4
Gabriel Jouet <gabriel.jouet@viacesi.fr>
2020-06-20 18:07:24 +0200

    Added a way to hide button and choose

---
+++ Assets/Scripts/BackgroudSelecter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class BackgroudSelecter : MonoBehaviour
+{
+    [SerializeField]
+    private InformationUIController _informationUIController;
+
+
+
+    private void OnMouseDown()
+    {
+        _informationUIController.DisableEnemyInformation();
+        _informationUIController.DisableTowerInformation();
+    }
+}
