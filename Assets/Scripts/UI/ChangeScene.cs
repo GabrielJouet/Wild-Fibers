73c06b6d1efb026b22e98483f5197a5b53c52ff7
Gabriel Jouet <gabriel.jouet@viacesi.fr>
2020-08-01 15:55:55 +0200

    Added a level selection screen and two level information
    By the way the second level will not be used now

--- Assets/Scripts/UI/ChangeScene.cs
+++ Assets/Scripts/UI/ChangeScene.cs
@@ -4,11 +4,17 @@ using UnityEngine.SceneManagement;
 public class ChangeScene : MonoBehaviour
 {
     [SerializeField]
-    private string _mapSceneName;
+    private string _destinationScene;
 
 
-    public void BackToMap()
+    public void LoadScene()
     {
-        SceneManager.LoadScene(_mapSceneName);
+        SceneManager.LoadScene(_destinationScene);
+    }
+
+
+    public void LoadScene(string destinationName)
+    {
+        SceneManager.LoadScene(destinationName);
     }
 }
---
+++ Assets/Scripts/UI/MapSelection/LevelButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class LevelButton : MonoBehaviour
+{
+    [SerializeField]
+    private LevelParameters _levelParameters;
+
+    [SerializeField]
+    private LevelSelection _levelSelection;
+
+    private void OnMouseDown()
+    {
+        _levelSelection.ActivateLevelSelectionMenu(_levelParameters);
+    }
+}
