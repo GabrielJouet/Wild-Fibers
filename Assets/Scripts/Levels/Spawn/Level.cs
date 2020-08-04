382e80313a3e718d2186be81b55ea3b4ea9ec829
Gabriel Jouet <gabriel.jouet@viacesi.fr>
2020-08-04 14:31:32 +0200

    Levels parameters are rewrote

--- Assets/Scripts/Levels/Spawn/Level.cs
+++ Assets/Scripts/Levels/Spawn/Level.cs
@@ -5,6 +5,22 @@ using UnityEngine;
 public class Level : ScriptableObject
 {
     [SerializeField]
+    private string _name;
+
+    [SerializeField]
+    private int _number;
+
+    [SerializeField]
+    private Sprite _picture;
+
+    [SerializeField]
+    private string _description;
+
+    [SerializeField]
+    private string _playSceneName;
+
+
+    [SerializeField]
     private List<Wave> _availableWaves;
 
 
@@ -14,4 +30,14 @@ public class Level : ScriptableObject
     public Wave GetWave(int index) { return _availableWaves[index]; }
 
     public int GetWaveCount() { return _availableWaves.Count; }
+
+    public string GetName() { return _name; }
+
+    public int GetNumber() { return _number; }
+
+    public Sprite GetPicture() { return _picture; }
+
+    public string GetDescription() { return _description; }
+
+    public string GetPlaySceneName() { return _playSceneName; }
 }
