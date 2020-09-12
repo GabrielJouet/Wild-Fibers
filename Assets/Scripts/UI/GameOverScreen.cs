7db40a2da9e249b2a31e3f0f0786c1d70ce9cdd6
Gabriel Jouet <gabriel.jouet@viacesi.fr>
2020-09-12 10:59:24 +0200

    Added commentaries on UI folder

--- Assets/Scripts/UI/GameOverScreen.cs
+++ Assets/Scripts/UI/GameOverScreen.cs
@@ -2,18 +2,29 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+/*
+ * Object used in game over behavior, show a game over screen
+ */
 public class GameOverScreen : MonoBehaviour
 {
+    //Text component that handles lose / win text
     [SerializeField]
     private Text _mainText;
+
+    //Game screen object
     [SerializeField]
     private GameObject _gameScreen;
 
+
+    //Pause controller used to stop everything
     [SerializeField]
     private PauseController _pauseController;
 
 
 
+    //Method used to activate game over screen
+    //
+    //Parameter => win, does the player win this level?
     public void Activate(bool win) 
     {
         _gameScreen.SetActive(true);
@@ -21,9 +32,10 @@ public class GameOverScreen : MonoBehaviour
     }
 
 
+    //Coroutine used to delay a bit game over screen popup
     private IEnumerator DelayShow(bool win)
     {
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(1f);
 
         if (win)
         {
