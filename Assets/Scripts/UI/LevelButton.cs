1e61a45f6ce49ed0d8ce291daad370e7b2d94a14
Gabriel Jouet <gabriel.jouet@viacesi.fr>
2020-09-13 18:43:07 +0200

    Added outline on level selection #179

--- Assets/Scripts/UI/LevelButton.cs
+++ Assets/Scripts/UI/LevelButton.cs
@@ -1,12 +1,47 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class LevelButton : MonoBehaviour
+public class LevelButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [Header("Components")]
     [SerializeField]
     private Level _levelParameters;
     [SerializeField]
     private LevelSelection _levelSelection;
+
+
+    [Header("Selection")]
+    [SerializeField]
+    private Image _hoverDisplayer;
+
+    [SerializeField]
+    private Sprite _unlockedHover;
+
+    [SerializeField]
+    private Sprite _finishedHover;
+
+    [SerializeField]
+    private Sprite _asideHover;
+
+    [SerializeField]
+    private Sprite _challengedHover;
+
+
+    public bool isOver = false;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _hoverDisplayer.gameObject.SetActive(true);
+        _hoverDisplayer.sprite = _unlockedHover;
+        _hoverDisplayer.SetNativeSize();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _hoverDisplayer.gameObject.SetActive(false);
+    }
+
 
 
     public void Activate()
