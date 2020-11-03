f16266f8cac63c5b8b63ddae4642078a3459580f
Gabriel Jouet <gabriel.jouet@viacesi.fr>
2020-11-03 21:47:25 +0100

    Added boss #252

---
+++ Assets/Scripts/Enemies/Boss.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Boss : Enemy
+{
+    [Header("Boss related")]
+    [SerializeField]
+    protected Enemy _enemySpawnedPrefab;
+
+    [SerializeField]
+    protected float _timeBetweenSpawn;
+
+    [SerializeField]
+    protected float _spawnTime;
+
+    [SerializeField]
+    protected bool _stopWhileSpawning;
+
+    [SerializeField]
+    protected int _numberOfEnemiesPerSpawn;
+
+
+    protected LevelController _levelController;
+
+
+
+    public override void Initialize(Path newPath, EnemyPool newPool, int pathIndex)
+    {
+        base.Initialize(newPath, newPool, pathIndex);
+        _levelController = FindObjectOfType<LevelController>();
+
+        StartCoroutine(DelaySpawn());
+    }
+
+
+    //Update method, called each frame
+    protected new void Update()
+    {
+        if (_moving)
+            FollowPath();
+    }
+
+
+
+    protected IEnumerator DelaySpawn()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(_timeBetweenSpawn);
+
+            _moving = false;
+            for(int i = 0; i < _numberOfEnemiesPerSpawn; i ++)
+            {
+                Enemy hatchling = _levelController.RecoverPool(_enemySpawnedPrefab).GetOneEnemy();
+                hatchling.Initialize(_path, _enemyPool, _pathIndex);
+                yield return new WaitForSeconds(_spawnTime);
+            }
+            _moving = true;
+        }
+    }
+}
