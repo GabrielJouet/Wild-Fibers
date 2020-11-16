e4304eeb5d80322a1e5d3f5aedc2664f36af807c
Gabriel Jouet <gabriel.jouet@viacesi.fr>
2020-11-16 19:54:28 +0100

    Added static depth manager

---
+++ Assets/Scripts/Controllers/StaticDepthManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+/*
+ * This class is used to render perspective in a 2D game
+ */
+public class StaticDepthManager : MonoBehaviour
+{
+    //Sprite renderer of the game object
+    [Header("Component")]
+    [SerializeField]
+    private SpriteRenderer _spriteRenderer;
+
+    //Main Camera of the Scene
+    private Camera _mainCamera;
+
+
+
+    //Start Method
+    //Called when the game object is initialized
+    private void Start()
+    {
+        _mainCamera = Camera.main;
+        _spriteRenderer.sortingOrder = (int)_mainCamera.WorldToScreenPoint(transform.position).y * -1;
+    }
+}
