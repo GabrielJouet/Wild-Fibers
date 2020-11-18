e2f27a2f35905caefcd72844f5c00d3d7f840c0a
Gabriel Jouet <gabriel.jouet@viacesi.fr>
2020-11-18 20:46:01 +0100

    Added basic particle behavior

--- Assets/Scripts/Particles/Particle.cs
+++ Assets/Scripts/Particles/Particle.cs
@@ -1,33 +1,54 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Particle : MonoBehaviour
 {
+    [Header("Graphical")]
     [SerializeField]
-    private float _liveTime;
+    protected List<Sprite> _particleSprites;
 
     [SerializeField]
-    private float _speed;
-
-    private ParticlePool _pool;
+    protected SpriteRenderer _spriteRenderer;
 
 
-    public void Initialize(float newLiveTime, float newSpeed, Vector3 newPosition)
-    {
-        transform.position = newPosition;
-        _liveTime = newLiveTime;
-        _speed = newSpeed;
-        StartCoroutine(Fade());
-    }
+    [Header("Behavior")]
+    [SerializeField]
+    protected float _liveTime;
+
+    [SerializeField]
+    protected float _horizontalMove;
+
+    [SerializeField]
+    protected float _verticalMove;
+
+
+    protected ParticlePool _pool;
+
+    protected float _horizontalSpeed;
+
+    protected float _verticalSpeed;
+
 
     public void Initialize(Vector3 newPosition)
     {
+        _spriteRenderer.sprite = _particleSprites[Random.Range(0, _particleSprites.Count)];
         transform.position = newPosition;
+
+        _horizontalSpeed = Random.Range(-_horizontalMove, _horizontalMove) * Time.deltaTime;
+        _verticalSpeed = Random.Range(-_verticalMove, _verticalMove) * Time.deltaTime;
+
         StartCoroutine(Fade());
     }
 
 
-    private IEnumerator Fade()
+    protected void Update()
+    {
+        transform.position = new Vector2(transform.position.x + _horizontalSpeed, transform.position.y + _verticalSpeed);
+    }
+
+
+    protected IEnumerator Fade()
     {
         yield return new WaitForSeconds(_liveTime);
         _pool.RecoverParticle(this);
