6962bdb79dcd2142477709a5139197ae62dcc671
Gabriel Jouet <gabriel.jouet@viacesi.fr>
2020-12-19 12:44:03 +0100

    Add an enhanced version of Depth manager #386

---
+++ Assets/Scripts/Controllers/Depth Manager/DepthManagerEnhanced.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Class used to control depth in 2D environnement.
+/// </summary>
+public class DepthManagerEnhanced : MonoBehaviour
+{
+    /// <summary>
+    /// The sprite renderer component that will be update.
+    /// </summary>
+    [SerializeField]
+    private SpriteRenderer _spriteRenderer;
+
+    /// <summary>
+    /// The transform component that will be used to check position.
+    /// </summary>
+    [SerializeField]
+    private Transform _transform;
+
+
+    /// <summary>
+    /// The main camera of the scene.
+    /// </summary>
+    private Camera _mainCamera;
+
+
+
+    /// <summary>
+    /// Awake method, used at first to initialize.
+    /// </summary>
+    private void Awake()
+    {
+        _mainCamera = Camera.main;
+    }
+
+
+    /// <summary>
+    /// Fixed Update is called 50 times a second.
+    /// </summary>
+    private void FixedUpdate()
+    {
+        _spriteRenderer.sortingOrder = (int)_mainCamera.WorldToScreenPoint(_transform.position).y * -1;
+    }
+}
