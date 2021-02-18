2554a23df5d34366ab946de3e8937f03c12cfa8f
Gabriel Jouet <gabriel.jouet@viacesi.fr>
2021-02-18 10:33:30 +0100

    Fixed Toxic Ivy randomness

--- Assets/Scripts/Towers/ToxicIvy.cs
+++ Assets/Scripts/Towers/ToxicIvy.cs
@@ -30,7 +30,7 @@ public class ToxicIvy : Tower
         {
             StopAllCoroutines();
 
-            List<Enemy> enemies = RecoverAvailableEnemies(_availableEnemies.Count < _availableLeafs.Count ? _availableEnemies.Count : _availableLeafs.Count, false);
+            List<Enemy> enemies = RecoverAvailableEnemies(_availableEnemies.Count < _availableLeafs.Count ? _availableEnemies.Count : _availableLeafs.Count, true);
 
             for (int i = 0; i < enemies.Count; i++)
                 _availableLeafs.Pop().StartFollowing(enemies[i], _towerData);
