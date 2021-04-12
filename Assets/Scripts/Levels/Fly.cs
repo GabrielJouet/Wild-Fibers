e97f5fe7025f5dd2e2b7bd0525c461857cac25f8
Gabriel Jouet <gabriel.jouet@viacesi.fr>
2021-04-12 18:24:33 +0200

    Added a little fly-like entity for light purpose

---
+++ Assets/Scripts/Levels/Fly.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Fly : MonoBehaviour
+{
+    [SerializeField]
+    private float _movementMax;
+
+
+    private Vector3 _goalPosition;
+
+    private float _speed;
+
+
+    private IEnumerator Start()
+    {
+        while (true)
+        {
+            _speed = Random.Range(0.01f, 0.15f);
+            _goalPosition = transform.position + new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f)) * _movementMax;
+            yield return new WaitUntil(() => transform.position == _goalPosition);
+        }
+    }
+
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _goalPosition, Time.deltaTime * _speed);
+    }
+}
