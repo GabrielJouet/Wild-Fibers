4d639a7f79e37954532216aa8523161007a549cf
Gabriel Jouet <gabriel.jouet@viacesi.fr>
2021-09-06 11:18:32 +0200

    Fixed wipe save button not working after scene change

---
+++ Assets/Scripts/UI/Parameters/WipeSave.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Class used to handles wipe save.
+/// </summary>
+public class WipeSave : MonoBehaviour
+{
+    /// <summary>
+    /// Method used to wipe the entire save.
+    /// </summary>
+    public void WipeEntireSave()
+    {
+        Controller.Instance.SaveControl.ResetData();
+    }
+}
