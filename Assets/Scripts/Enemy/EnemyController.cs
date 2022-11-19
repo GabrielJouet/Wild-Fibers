3f4e1aa52db9c9883449c2d145cbd35b89c6f445
Gabriel Jouet <gabriel.jouet@viacesi.fr>
2022-11-19 16:39:28 +0100

    Added commentaries on enemy controller

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -9,16 +9,20 @@ public class EnemyController : MonoBehaviour
     /// <summary>
     /// All enemies in a list format.
     /// </summary>
-    public List<Enemy> Enemies { get => _allEnemies; }
     [SerializeField]
     private List<Enemy> _allEnemies;
+
+    /// <summary>
+    /// All enemies in a list format, accessor.
+    /// </summary>
+    public List<Enemy> Enemies { get => _allEnemies; }
 
 
 
     /// <summary>
-    /// 
+    /// Method called when an enemy reaches the end of the path.
     /// </summary>
-    /// <param name="livesLost"></param>
+    /// <param name="livesLost">How many lives are lost?</param>
     public void ReachEnd(int livesLost)
     {
         FindObjectOfType<RessourceController>().RemoveLives(livesLost);
@@ -26,9 +30,9 @@ public class EnemyController : MonoBehaviour
 
 
     /// <summary>
-    /// 
+    /// Method called when an enemy dies.
     /// </summary>
-    /// <param name="goldGained"></param>
+    /// <param name="goldGained">How much gold is gained?</param>
     public void Die(int goldGained)
     {
         FindObjectOfType<RessourceController>().AddGold(goldGained, true);
