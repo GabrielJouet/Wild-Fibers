130362d1885d0bb9a399e7d3f6bf7f8b18dabce7
GabrielJouet <gabriel.jouet@viacesi.fr>
2023-10-24 16:56:32 +0200

    feat: Changed how hider works + Changed how reset save works

--- Assets/Scripts/UI/Hider.cs
+++ Assets/Scripts/UI/Hider.cs
@@ -1,5 +1,4 @@
-﻿using Levels;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace UI
 {
@@ -14,48 +13,13 @@ namespace UI
         [SerializeField]
         private DisplayController displayController;
 
+        
         /// <summary>
-        /// Level controller component used to know if the game is finished.
+        /// Method called when the button is clicked.
         /// </summary>
-        [SerializeField]
-        private LevelController levelController;
-
-        /// <summary>
-        /// Box collider component used to resize to the size of the screen
-        /// </summary>
-        [SerializeField] 
-        private BoxCollider2D boxCollider;
-
-
-        /// <summary>
-        /// Actual camera used.
-        /// </summary>
-        private Camera _camera;
-
-
-
-        /// <summary>
-        /// Method used as initialization.
-        /// </summary>
-        private void Start()
+        public void ResetDisplay()
         {
-            _camera = Camera.current;
-            boxCollider.size = new Vector2(Screen.width, Screen.height);
-        }
-
-
-        /// <summary>
-        /// Update method, called each frame.
-        /// </summary>
-        private void Update()
-        {
-            if (Input.GetMouseButtonUp(0))
-            {
-                RaycastHit2D hit = Physics2D.Raycast(_camera.ScreenToWorldPoint(Input.mousePosition), Vector3.forward, 90);
-
-                if (hit.collider && hit.collider.GetComponent<Hider>() && (levelController || !levelController.Ended))
-                    displayController.ResetDisplay();
-            }
+            displayController.ResetDisplay();
         }
     }
 }
