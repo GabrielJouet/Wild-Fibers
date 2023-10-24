bdb50c4f396e45b3f7e1f7d83d353dc60e0fb986
GabrielJouet <gabriel.jouet@viacesi.fr>
2023-10-24 11:27:43 +0200

    feat: Cleaned UI Parameters scripts

--- Assets/Scripts/UI/Parameters/MusicSlider.cs
+++ Assets/Scripts/UI/Parameters/MusicSlider.cs
@@ -2,53 +2,56 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-/// <summary>
-/// Class used to handle music and sound levels.
-/// </summary>
-[RequireComponent(typeof(Slider))]
-public class MusicSlider : MonoBehaviour
+namespace UI.Parameters
 {
     /// <summary>
-    /// Does the slider is used for sound or music?
+    /// Class used to handle music and sound levels.
     /// </summary>
-    [SerializeField]
-    private bool _sound;
-
-
-    /// <summary>
-    /// Save controller component.
-    /// </summary>
-    private SaveController _saveController;
-
-    /// <summary>
-    /// Slider component.
-    /// </summary>
-    private Slider _slider;
-
-
-
-    /// <summary>
-    /// Method called when the object is activated.
-    /// </summary>
-    private void OnEnable()
+    [RequireComponent(typeof(Slider))]
+    public class MusicSlider : MonoBehaviour
     {
-        _saveController = Controller.Instance.SaveController;
-        _slider = GetComponent<Slider>();
-        _slider.value = _sound ? _saveController.SaveFile.Sound : _saveController.SaveFile.Music;
-    }
+        /// <summary>
+        /// Does the slider is used for sound or music?
+        /// </summary>
+        [SerializeField]
+        private bool sound;
 
 
-    /// <summary>
-    /// Method called each time a slider has a new value.
-    /// </summary>
-    public void ChangeSliderValue()
-    {
-        if (_saveController != null)
+        /// <summary>
+        /// Save controller component.
+        /// </summary>
+        private SaveController _saveController;
+
+        /// <summary>
+        /// Slider component.
+        /// </summary>
+        private Slider _slider;
+
+
+
+        /// <summary>
+        /// Method called when the object is activated.
+        /// </summary>
+        private void OnEnable()
         {
-            if (_sound)
-                _saveController.SaveSoundLevel(_slider.value);
-            else
-                _saveController.SaveMusicLevel(_slider.value);
+            _saveController = Controller.Instance.SaveController;
+            _slider = GetComponent<Slider>();
+            _slider.value = sound ? _saveController.SaveFile.Sound : _saveController.SaveFile.Music;
+        }
+
+
+        /// <summary>
+        /// Method called each time a slider has a new value.
+        /// </summary>
+        public void ChangeSliderValue()
+        {
+            if (_saveController != null)
+            {
+                if (sound)
+                    _saveController.SaveSoundLevel(_slider.value);
+                else
+                    _saveController.SaveMusicLevel(_slider.value);
+            }
         }
     }
 }
